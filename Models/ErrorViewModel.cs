30feaa49e89b5a88a5465648c897f1e5a1d4db05
Adrian <adriankelvinr288@gmail.com>
2026-03-19 15:36:24 -0400

    Proyecto inicial MVC taller mecanico

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TallerMecanico.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
